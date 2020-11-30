bdee2aab097006491b65c7aedf5632e8226a73d6
Luciano Gomes Vieira dos Anjos <62733659+lucianoanjos02@users.noreply.github.com>
2020-11-30 07:41:30 -0300

    Implementação de Login + Cadastro de Usuário
    - Implementação de Login e Autenticação da Sessão do Usuário com Flask-Login
    
    - Implementação da view de Cadastro de Usuário + métodos de cadastro da classe UserDAO

--- modules/models/user.py
+++ modules/models/user.py
@@ -1,7 +1,8 @@
 from modules.db.db import Base
+from flask_login import UserMixin
 from sqlalchemy import Column, String, Integer
 
-class User(Base):
+class User(Base, UserMixin):
     __tablename__ = 'TUser'
     user_id = Column(Integer, primary_key=True, autoincrement=True)
     user_first_name = Column(String(15), nullable=False)
@@ -16,3 +17,18 @@ class User(Base):
         self.user_login = user_login
         self.user_password = user_password
         self.user_email = user_email
+    
+    @property
+    def is_authenticated(self):
+        return True
+    
+    @property
+    def is_active(self):
+        return True
+    
+    @property
+    def is_anonymous(self):
+        return False
+    
+    def get_id(self):
+        return str(self.user_id)
